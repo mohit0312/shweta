b80d3d376a44f73ac794ce8ffe557f64e14664ad
goelShweta <31646342+goelShweta@users.noreply.github.com>
2017-10-24 12:46:37 +0530

    Add files via upload

--- shweta.py
+++ shweta.py
@@ -1,22 +1,30 @@
-'''
-    objective: To flatten the list
-    input parameters: the initial list to be flatten
-                      the list in which the result to be stored
-    result: the flattened list        
-'''
-def list_fun(list1,list2,i):
-    if i==len(list1) :
-        return list2
-    elif type(list1[i])==list :
-        list2.extend(list1[i])
-        return list_fun(list1,list2,i+1)
-    else:
-        list2.append(list1[i])
-        return list_fun(list1,list2,i+1)
-    #i=i+1
-    #return(list_fun(list1,list2,i))
+def insert_fun(num,list1):
+    ''' Objective: to insert a no. in a sorted list
+        input parameter: a no. to be inserted
+        output: a list after adding the desired no.
+    '''
+    #approach: using recursion
     
-l=[2,3,[8,4],7,[9,1]] 
-m=[]
-i=0
-list_fun(l,m,i)   
+    length = len(list1)
+    print(length)
+    if length == 0:
+        list1.append(num)
+        return list1
+    else:
+        for i in range (0,length-1) :
+            if(list1[i] < num) :
+                i+=1
+        i=i+1    
+        ele = list1[i]
+        list1[i]=num
+        num=ele
+        insert_fun(num,list1[1:])
+            
+
+    return list1
+
+listp = [1,2,3,5,6]
+value = 4
+
+insert_fun(value, listp)
+
