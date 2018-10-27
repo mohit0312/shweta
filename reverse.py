d021f80303317bd3802c4117bcd988b5035fff17
kalpanayadav <31645538+kalpanayadav@users.noreply.github.com>
2018-10-27 22:16:34 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,10 +1,14 @@
 #reverse function
 
 list2=[]
+'''
+    empty list
+  '''
 def rec(list1):
     length=len(list1)
     if list1==[]:
         return list2
+    
     else:
         list2.append(list1[length-1])
         return rec(list1[0:length-1])
